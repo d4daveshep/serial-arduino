da9130905563083ac30da8250991d0b4aef8c9b1
David Shepherd <david@daveshep.net.nz>
2018-01-04 09:03:48 +1300

    fixed timestamp format

--- SerialReadToElastic.py
+++ SerialReadToElastic.py
@@ -9,13 +9,16 @@ import elasticsearch
 from elasticsearch import Elasticsearch
 
 # connect to Elasticsearch
-eshost = 'search-test-fermenter-temp-ac27nb3jxwgsv6m6zfpjpsprsa.us-west-2.es.amazonaws.com'
-es = Elasticsearch(
-    hosts=[{'host':eshost, 'port':443}],
-    use_ssl=True,
-    verify_certs=True,
-    connection_class=elasticsearch.connection.RequestsHttpConnection
-)
+eshost = 'search-test-fermenter-temp-ac27nb3jxwgsv6m6zfpjpsprsa.us-west-2.es.amazonaws.com' 
+#es = Elasticsearch(
+#    hosts=[{'host':eshost, 'port':443}],
+#    use_ssl=True,
+#    verify_certs=True,
+#    sniff_on_start=True,
+#    sniffer_timeout=60,
+#    sniff_on_connection_fail=True,
+#    connection_class=elasticsearch.connection.RequestsHttpConnection
+#)
 #print( es.cluster.health() )
 
 # find the serial port
@@ -41,17 +44,24 @@ while True:
     data = json.loads(line.decode('utf-8')) # convert serial line to string and load to JSON
     stamp = datetime.now() # get timestamp and format with 0 millisecs
 
-    data['timestamp'] = stamp.strftime('%Y-%m-%d %H:%M:%S.0')
+    data['timestamp'] = stamp.strftime('%Y-%m-%dT%H:%M:%S.0+13')
 #    data['brewid'] = '12-AAA-02'
     data['brewid'] = '99-TEST-99'
 
     doc = data
     print( json.dumps(doc) )
+
+    es = Elasticsearch(
+        hosts=[{'host':eshost, 'port':443}],
+        use_ssl=True,
+        verify_certs=True,
+        connection_class=elasticsearch.connection.RequestsHttpConnection
+    )
     
     # index the doc to elastic
     res = es.index( 
-        index="test-brews", 
-        doc_type="temp-reading", 
+        index="mapping-test", 
+        doc_type="brew-temp", 
         body=doc
     )
     print(json.dumps(res))
